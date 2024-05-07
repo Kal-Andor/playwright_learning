2e5727979ad7fec0758bd72fe82462ad06b7fd74
Andor Szabó <szandor.92@gmail.com>
2024-05-07 13:46:16 +0300

    goto given url

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -4,5 +4,6 @@ test('First Playwright test', async ({browser})=>
     {
         const context = await browser.newContext();
         const page = await context.newPage();
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
 
     });
