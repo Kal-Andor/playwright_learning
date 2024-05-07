d9336441b93d94fd2ce11bf5cb3af94a907c8a6d
Andor Szabó <szandor.92@gmail.com>
2024-05-07 13:45:39 +0300

    context and page initalising skeleton

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,6 +1,8 @@
 const {test} = require('@playwright/test')
 
-test('First Playwright test', async ()=>
+test('First Playwright test', async ({browser})=>
     {
+        const context = await browser.newContext();
+        const page = await context.newPage();
 
     });
